79a226b410a857818da942247297c90292f60eef
wcq <3@q.com>
2023-02-09 17:58:56 +0800

    使用mac的蓝牙功能

--- mac/mac_startup.sh
+++ mac/mac_startup.sh
@@ -4,4 +4,4 @@
 sudo -S kextunload -b com.apple.driver.AppleHIDKeyboard > /dev/null
 
 ## disable bluetooth --> cause crash
-sudo launchctl unload /System/Library/LaunchDaemons/com.apple.bluetoothd.plist
+# sudo launchctl unload /System/Library/LaunchDaemons/com.apple.bluetoothd.plist
